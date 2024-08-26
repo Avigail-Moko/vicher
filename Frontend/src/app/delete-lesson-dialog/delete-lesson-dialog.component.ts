f0e2dff6c02483493c61f80067d238b978b648d2
Avigail-Moko <avigailmoko@gmail.com>
2024-08-26 16:29:15 +0300

    fix

--- Frontend/src/app/delete-lesson-dialog/delete-lesson-dialog.component.ts
+++ Frontend/src/app/delete-lesson-dialog/delete-lesson-dialog.component.ts
@@ -7,12 +7,18 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./delete-lesson-dialog.component.scss']
 })
 export class DeleteLessonDialogComponent {
+  userProfile = JSON.parse(localStorage.getItem('userProfile'));
   lesson:any;
+  partner: any;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,public dialogRef: MatDialogRef<DeleteLessonDialogComponent>,){
     this.lesson=data.lesson
   }
   closeDialog(result: boolean): void {
     this.dialogRef.close(result);
   }
- 
+  ngOnInit() {
+    const teacher_name= this.lesson.teacher_name
+    const student_name= this.lesson.student_name
+    this.partner=this.userProfile.name===teacher_name?student_name:teacher_name;
+  }
 }
